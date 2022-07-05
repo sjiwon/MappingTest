bbf1c70578ccb5f6c3a2e4aeb40e2ee14fb052ea
SeoJiWon <sjiwon4491@gmail.com>
2022-07-05 19:26:32 +0900

    remove files

--- src/main/java/AA/MappingTest/repository/LikeArtistRepository.java
+++
@@ -1,7 +0,0 @@
-package AA.MappingTest.repository;
-
-import AA.MappingTest.domain.LikeArtist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeArtistRepository extends JpaRepository<LikeArtist, Long> {
-}
